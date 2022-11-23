75ca9130347809b41d573b9dacc6029736c0f00c
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-23 14:05:26 -0500

    Uso de slots.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ components/header.component.js
@@ -0,0 +1,7 @@
+export default {
+    template:`
+    <header>
+        <slot></slot>
+    </header>
+    `
+}
--- components/test-comopnents.js
+++ components/test-comopnents.js
@@ -1,11 +1,16 @@
+import HeaderComponent from './header.component.js'
 import CounterButtonComponent from './counter-button.component.js'
 
 export default {
     components: {
+        HeaderComponent,
         CounterButtonComponent,
     },
     template: `
     <div class="container">
+        <HeaderComponent>
+            <h2>Prueba de components</h2>
+        </HeaderComponent>
         <CounterButtonComponent :counter="0" />
         <CounterButtonComponent :counter="10" />
     </div>
